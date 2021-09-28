eb04740b98a825385068073f648ed25bd50c60b8
Richard Sprague <richardsprague@gmail.com>
2021-09-28 05:51:42 -0700

    This version failed with Docker load error.

--- global.R
+++ global.R
@@ -5,6 +5,7 @@ library(lubridate)
 library(DBI)
 library(RPostgres)
 library(cgmr)
+library(psiCGM)
 
 Sys.setenv(R_CONFIG_ACTIVE = "shinyapps")
 
@@ -18,7 +19,11 @@ con <- DBI::dbConnect(
   password = conn_args$password
 )
 
+GLUCOSE_RECORDS<- tbl(con,"glucose_records") %>% collect()
+NOTES_RECORDS <- tbl(con, "notes_records") %>% collect()
 
 source("mod_CSV.R")
 source("psi_plot.R")
+taster_foods <- food_list_db(user_id = c(1001:1004,1007:1021))
+source("mod_foodTaster_compare.R")
 
