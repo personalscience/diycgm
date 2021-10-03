5ef41308243b9e1a78a2a85014884776b685bce6
Richard Sprague <richardsprague@gmail.com>
2021-10-03 07:19:39 -0700

    barebones...relies just on package tastermonial.

---
+++ app.R
@@ -0,0 +1,4 @@
+
+library(tastermonial)
+options( "golem.app.prod" = TRUE)
+tastermonial::run_app() # add parameters here (if any)
--- psi_deploy.R
+++ psi_deploy.R
@@ -7,6 +7,11 @@
 
 ### Deploy to Polished
 
+remove.packages("tastermonial")
+devtools::install_github("personalscience/taster",
+                         ref = "a5d6649977c3177c48f8a7c8ec7b067924ab1e0e",
+                         upgrade = "never") #577dc4100cac3940") #,
+
 remotes::install_github("tychobra/polished")
 
 polished::deploy_app(
